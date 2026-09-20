74388581b9fdf1dbcd2bc6814463dcca432d87d0
Swanik Srivastava <167602492+Asceptic07@users.noreply.github.com>
2026-09-20 11:05:29 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int helper(vector<int> &cost,int i)
+    {
+        if(i==0 || i==1) return cost[i]; 
+        return cost[i]+min(helper(cost,i-1),helper(cost,i-2));
+    }
+
+    int helper2(vector<int> &cost,int i,vector<int> &dp)
+    {
+        if(i==0 || i==1) return cost[i];
+        if(dp[i]!=-1)
+        {
+            return dp[i];
+        }
+        return dp[i]=cost[i]+min(helper2(cost,i-1,dp),helper2(cost,i-2,dp));
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) 
+    {
+        int n=cost.size();
+        //return min(helper(cost,n-1),helper(cost,n-2));
+        
+        vector<int> dp(n,-1);
+        return min(helper2(cost,n-1,dp),helper2(cost,n-2,dp));
+        
+    }
+};
